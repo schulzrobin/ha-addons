68524a3f0ff2282fb4352a8064c637029ab42a61
Robin Schulz <106838867+schulzrobin@users.noreply.github.com>
2025-08-11 19:27:01 +0200

    Aktualisieren von run.sh

--- shelly-firmware-updater/run.sh
+++ shelly-firmware-updater/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/with-contenv bashio
 set -e
-
-# Gunicorn auf den Ingress-Port
+# Gunicorn läuft in der venv (PATH wurde im Dockerfile gesetzt)
 exec gunicorn -b 0.0.0.0:8099 app:app --workers 4 --threads 2 --timeout 90
