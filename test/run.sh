09ef00b6c80f364be194048be3c942fc9b384fd8
Robin Schulz <106838867+schulzrobin@users.noreply.github.com>
2025-08-13 23:06:00 +0200

    Aktualisieren von run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
+CONFIG_PATH=/data/options.json
+
+TARGET="$(bashio::config 'target')"
+
 echo "Hello world!"
+echo "Version: 1.0.3"
 echo "server starten..."
 python3 -m http.server 8099
