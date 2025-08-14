b5403a3a12fd66f85970a56f96115fef0cd7742a
Robin Schulz <106838867+schulzrobin@users.noreply.github.com>
2025-08-14 20:18:30 +0200

    Aktualisieren von run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-CONFIG_PATH=/data/options.json
-TARGET="$(bashio::config 'target')"
-
-ADDON_VERSION=$(jq -r '.version' /data/options.json 2>/dev/null || echo "unknown")
 
 echo "Hello world!"
 echo "server starten..."
-echo "Version: $ADDON_VERSION"
 python3 -m http.server 8099
