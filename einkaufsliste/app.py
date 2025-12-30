33769598069d660de169521a765db1d2d8a617c3
Robin Schulz <106838867+schulzrobin@users.noreply.github.com>
2025-12-30 22:59:00 +0100

    Pass items to index.html in render_template

--- einkaufsliste/app.py
+++ einkaufsliste/app.py
@@ -78,7 +78,7 @@ def delete(item_id):
         return "Database error", 500
     finally:
         conn.close()
-    return render_template("index.html")
+    return render_template("index.html", items=items)
 
 
 if __name__ == "__main__":
