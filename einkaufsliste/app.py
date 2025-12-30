c5d2daca7dcca6d43043224a6b86bc6149f16078
Robin Schulz <106838867+schulzrobin@users.noreply.github.com>
2025-12-30 22:57:48 +0100

    Change redirect to render index.html template

--- einkaufsliste/app.py
+++ einkaufsliste/app.py
@@ -78,7 +78,7 @@ def delete(item_id):
         return "Database error", 500
     finally:
         conn.close()
-    return redirect('/')
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
