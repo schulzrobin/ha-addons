76e8991b319f14bd541b3bb22bcbad4c326b8604
Robin Schulz <106838867+schulzrobin@users.noreply.github.com>
2025-12-30 22:52:38 +0100

    Fix redirect URL in app.py

--- einkaufsliste/app.py
+++ einkaufsliste/app.py
@@ -78,7 +78,7 @@ def delete(item_id):
         return "Database error", 500
     finally:
         conn.close()
-    return redirect(url_for('index.html'))
+    return redirect(url_for('/'))
 
 
 if __name__ == "__main__":
